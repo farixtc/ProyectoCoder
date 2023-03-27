b62371639fd4471d022dec4e12f8b7807f438d50
Farid Tanta <faridtanta06@gmail.com>
2023-03-26 21:25:35 -0500

    help

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from AppCoder.views import *
 urlpatterns = [
     path('cursos', cursos, name="AppCoderCursos"),
+    path('buscar_curso', busqueda_curso, name="AppCoderBuscarCurso"),
     path('curso/<nombre>/<camada>', crear_curso, name="AppCoderCurso"),
     path('estudiantes', estudiantes, name="AppCoderEstudiantes"),
     path('profesores', profesores, name="AppCoderProfesores"),
